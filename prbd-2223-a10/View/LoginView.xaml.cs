2d697821ada3432508035ef3c9581897d861e785
Senol KACAR <1512sekacar@student.epfc.eu>
2023-03-19 17:29:18 +0100

    feat(LoginView): moving content of mainview to loginview

---
+++ prbd-2223-a10/View/LoginView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using PRBD_Framework;
+
+namespace MyPoll.View;
+
+public partial class LoginView : WindowBase {
+    public LoginView() {
+        InitializeComponent();
+    }
+
+    private void btnCancel_Click(object sender,RoutedEventArgs e) {
+        Close();
+    }
+}
---
+++ prbd-2223-a10/ViewModel/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyPoll.Model;
+using PRBD_Framework;
+
+namespace MyPoll.ViewModel;
+
+public class LoginViewModel : ViewModelBase<User, MyPollContext> {
+    protected override void OnRefreshData() {
+
+    }
+}
