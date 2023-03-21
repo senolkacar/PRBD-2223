acc58cf2fdd26fbbc803d5b6c3e7a3cfa2e3785b
Senol KACAR <1512sekacar@student.epfc.eu>
2023-03-21 17:39:24 +0100

    feat(PollCardView): start of implementing PollCardView

---
+++ prbd-2223-a10/ViewModel/PollCardViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyPoll.Model;
+using PRBD_Framework;
+
+namespace MyPoll.ViewModel;
+    public class PollCardViewModel : ViewModelCommon {
+    private readonly Poll _poll;
+
+    public Poll Poll {
+        get => _poll;
+        private init => SetProperty(ref _poll, value);
+    }
+
+    public string Name => Poll.Name;
+    public string Creator => Poll.Creator.FullName;
+
+    public PollCardViewModel(Poll poll) {
+        _poll = poll;
+    }
+}
